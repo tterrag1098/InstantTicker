e60c4b21531bed374d9a814d090c5bb02b230ac1
tterrag <tterrag1098@gmail.com>
2021-06-20 18:43:14 -0400

    Fix ticker pattern not requiring an ending word boundary

--- src/main/java/com/tterrag/iticker/util/Patterns.java
+++ src/main/java/com/tterrag/iticker/util/Patterns.java
@@ -5,8 +5,7 @@ import java.util.regex.Pattern;
 import com.tterrag.iticker.util.annotation.NonNullFields;
 
 @NonNullFields
-@SuppressWarnings("null")
 public class Patterns {
 
-    public static final Pattern TICKER = Pattern.compile("(?:^|\\b|\\s)(?:\\$(?=[^^])|(?=\\^))(\\^?[A-Z0-9.=\\-]*[A-Z0-9])(\\+?|\\b)");
+    public static final Pattern TICKER = Pattern.compile("(?:^|\\b|\\s)(?:\\$(?=[^^])|(?=\\^))(\\^?[A-Z0-9.=\\-]*[A-Z0-9])(\\+|\\b)");
 }
